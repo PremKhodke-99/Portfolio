f93c757717ecf83b649b106f8d7ade44716f1e0c
Prem <premkhodke.99@gmail.com>
2024-09-04 01:58:22 +0530

    navlinks constants

---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+export const navbarItem = [
+    {
+        title: 'Home',
+        href: '#home'
+    },
+    {
+        title: 'About',
+        href: '#about'
+    },
+    {
+        title: 'Projects',
+        href: '#projects'
+    },
+    {
+        title: 'Contact',
+        href: '#contact'
+    },
+    
+]
