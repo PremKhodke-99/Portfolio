7abc31dafe623c436c03954a09686f249bd38efe
Prem <premkhodke.99@gmail.com>
2024-09-04 17:34:58 +0530

    icons constant

--- constants/index.ts
+++ constants/index.ts
@@ -1,19 +1,97 @@
+import Html from "../public/assets/icons/html.svg";
+import Css from "../public/assets/icons/css.svg";
+import JavaScript from "../public/assets/icons/javascript.svg";
+import React from "../public/assets/icons/react.svg";
+import Node from "../public/assets/icons/nodejs.svg";
+import Express from "../public/assets/icons/express.svg";
+import MongoDB from "../public/assets/icons/mongodb.svg";
+import Next from "../public/assets/icons/next-js.svg";
+import Redux from "../public/assets/icons/redux.svg";
+import TypeScript from "../public/assets/icons/typescript.svg";
+import Tailwind from "../public/assets/icons/tailwindcss.svg";
+import Bootstrap from "../public/assets/icons/bootstrap.svg";
+import MaterialUI from "../public/assets/icons/material-ui.svg";
+import Vite from "../public/assets/icons/vitejs.svg";
+import Postman from "../public/assets/icons/postman.svg";
+
+export const logo = [
+  {
+    name: "html",
+    icon: Html,
+  },
+  {
+    name: "css",
+    icon: Css,
+  },
+  {
+    name: "javascript",
+    icon: JavaScript,
+  },
+  {
+    name: "react",
+    icon: React,
+  },
+  {
+    name: "node",
+    icon: Node,
+  },
+  {
+    name: "express",
+    icon: Express,
+  },
+  {
+    name: "mongodb",
+    icon: MongoDB,
+  },
+  {
+    name: "nextjs",
+    icon: Next,
+  },
+  {
+    name: "redux",
+    icon: Redux,
+  },
+  {
+    name: "typescript",
+    icon: TypeScript,
+  },
+  {
+    name: "tailwind",
+    icon: Tailwind,
+  },
+  {
+    name: "bootstrap",
+    icon: Bootstrap,
+  },
+  {
+    name: "materialui",
+    icon: MaterialUI,
+  },
+  {
+    name: "vite",
+    icon: Vite,
+  },
+  {
+    name: "postman",
+    icon: Postman,
+  },
+];
+
 export const navbarItem = [
-    {
-        title: 'Home',
-        href: '#home'
-    },
-    {
-        title: 'About',
-        href: '#about'
-    },
-    {
-        title: 'Projects',
-        href: '#projects'
-    },
-    {
-        title: 'Contact',
-        href: '#contact'
-    },
-    
-]
+  {
+    title: "Home",
+    href: "#home",
+  },
+  {
+    title: "About",
+    href: "#about",
+  },
+  {
+    title: "Projects",
+    href: "#projects",
+  },
+  {
+    title: "Contact",
+    href: "#contact",
+  },
+];
