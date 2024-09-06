f2fde6c8921c6a4d8a1a9534af3766768510ea68
Prem <premkhodke.99@gmail.com>
2024-09-06 11:55:26 +0530

    Link button on cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,11 @@
-import { url } from "inspector";
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import Arrow from '../../public/assets/arrow-right-up-line.svg'
 
 interface ProjectType {
   title: string;
@@ -11,21 +15,39 @@ interface ProjectType {
 }
 
 const Card = ({ title, description, image, href }: ProjectType) => {
+  const [isHover, setIsHover] = useState(false);
+
+  useGSAP(() => {
+    gsap.from("div div h1 #a", {
+      y: 70,
+      opacity: 1,
+      duration: 0.5,
+      stagger: 0.2,
+    });
+  }, [isHover]);
+
   return (
     <div
       className={`col-span-6 place-self-center w-full lg:h-[300px] h-[225px] box-shadow rounded-xl relative overflow-hidden bg-black`}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
     >
       <Image src={image} alt={title} fill className="scale-[1.01]" />
       <div className="w-full h-full bg-black absolute opacity-0 hover:opacity-90 duration-500 ease-in-out flex flex-col justify-center items-center">
-        <h1 className="text-emerald-50 uppercase text-6xl font-bold ">
-          {title}
+        <h1 className="text-emerald-50 uppercase text-6xl font-bold font-sans">
+          {title.split("").map((letter, idx) => (
+            <span key={idx} id="a">
+              {letter}
+            </span>
+          ))}
         </h1>
         <Link
           href={href}
           target="_blank"
-          className="text-white font-light gap-40 mt-16 border-[0.5px] border-gray-300 px-4 py-2 rounded-full hover:text-black hover:bg-emerald-50 duration-300"
+          className="text-white font-light mt-16 border-[0.5px] border-gray-300 px-4 py-2 rounded-full hover:text-black hover:bg-emerald-50 duration-300 flex gap-2"
         >
-          Link
+          <Image src={Arrow} alt="arrow" width={20} height={20} className="text-white"/>
+          <p>Link</p>
         </Link>
       </div>
     </div>
