803f0854a632d4227ba838ef42ab4f16315aef46
Prem <premkhodke.99@gmail.com>
2024-09-06 21:03:55 +0530

    height change

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-screen md:h-[40rem] bg-black relative flex items-center w-full justify-center overflow-hidden",
+        "h-screen bg-black relative flex items-center w-full justify-center overflow-hidden",
         // h-screen if you want bigger
         className
       )}
