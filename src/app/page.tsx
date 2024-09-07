f12a4717e6fb1d609a2c4454a08089da3e9dc794
Prem <premkhodke.99@gmail.com>
2024-09-07 14:32:08 +0530

    Snackbar provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import About from "@/sections/About";
 import Contact from "@/sections/Contact";
 import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Projects from "@/sections/Projects";
+import { SnackbarProvider } from "notistack";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-br from-yellow-100 to-emerald-100 overflow-x-hidden">
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </main>
+    <SnackbarProvider
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "center",
+      }}
+    >
+      <main className="bg-gradient-to-br from-yellow-100 to-emerald-100 overflow-x-hidden">
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </main>
+    </SnackbarProvider>
   );
 }
