3971b8010a6ebb3d1b3614566f0cd5e9fd2c2a16
Prem <premkhodke.99@gmail.com>
2024-09-07 14:34:33 +0530

    server actions

---
+++ src/actions/index.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { MessageType } from "@/custom";
+import { Resend } from "resend";
+import MailTemplate from "../../template/MailTemplate";
+import { ReactElement } from "react";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const toEmail: string = process.env.EMAIL!;
+
+export async function sendMessage(messageDetails: MessageType) {
+  const { email, subject, message } = messageDetails;
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: [toEmail],
+      subject: subject,
+      react: MailTemplate({ email, message }) as ReactElement,
+    });
+
+    if (data) {
+      return {
+        success: true,
+        message: "Message sent successfully!",
+      };
+    }
+
+    if (error) {
+      throw new Error();
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message: "Some error occured.",
+    };
+  }
+}
